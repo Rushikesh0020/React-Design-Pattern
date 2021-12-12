92825d6e891aae700bfae117bced852c6636d01a
Rushikesh0020 <rushikeshpatil0020@gmail.com>
2021-12-12 14:08:34 +0530

    DataSource Component

--- src/App5.js
+++ src/App5.js
@@ -4,23 +4,21 @@
 
 import axios from "axios";
 import { DataSource } from "./ContainerComponents/DataSource";
-import { UserInfo } from './ContainerComponents/UserInfo';
+import { UserInfo } from "./ContainerComponents/UserInfo";
 
-
-const getServerData = (url) => async () =>  {
-    const response = await axios.get(url);
-    return response.data;
-}
+const getServerData = (url) => async () => {
+  const response = await axios.get(url);
+  return response.data;
+};
 
 function App5() {
-    return (
-        <>
-            <DataSource getDataFunc={getServerData("/users/126")} resourceName="user">
-                <UserInfo />
-            </DataSource>
-        </>
-        
-       
-    )}
+  return (
+    <>
+      <DataSource getDataFunc={getServerData("/users/126")} resourceName="user">
+        <UserInfo />
+      </DataSource>
+    </>
+  );
+}
 
 export default App5;
